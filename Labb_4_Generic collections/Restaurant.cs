549e4a7b32f2db3838a26498b201c4b8bf3660f2
VISORKILLEN <viktor.gustavsson@live.se>
2025-11-05 14:37:47 +0100

    made a few more methods in restaurant class

--- Labb_4_Generic collections/Restaurant.cs
+++ Labb_4_Generic collections/Restaurant.cs
@@ -11,11 +11,28 @@
             _orders = new Queue<Order>();
         }
 
+        //Method to add new meal to the menu
         public void AddToMenu(MenuItem menuItem)
         {
             _menu.Add(menuItem);
             Console.WriteLine($"Ny rätt tillagd: {menuItem}");
         }
 
+        //Mehtod to show menu and a loop so it shows everything
+        public void ShowMenu()
+        {
+            Console.WriteLine("\n -- Meny --");
+            foreach (var item in _menu)
+            {
+                Console.WriteLine($"{item.Id}. {item}");
+            }
+        }
+
+        //Method to create new order
+        public void CreateOrder(Order order)
+        {
+            _orders.Enqueue(order);
+            Console.WriteLine($"Ny beställning gjord: {order}");
+        }
     }
 }
